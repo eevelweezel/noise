de73e1453f3c45e4eec83f64194659520ec4d3e2
Heather Delia White <white.heather33@gmail.com>
2019-04-17 23:36:30 -0500

    update a few comments

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import nussl
 from audioread import NoBackendError
 from gtts import gTTS
 
-def process_audio():
+def process_audio(lyrics):
     """
-    Separate audio file.
-    So far, Repet shows the most promise.
+    :lyrics -  gtts audio generated from lyrics
+    :a_file - user input of path/to/audio.file
+
+    Step 1: separate audio file (Repet shows the most promise)
+    - need to play w/ masks, threshholds
+
+    Step 2: apply a stft to lyrics based on fg, apply this to bg
+
+    Step 3: profit.
     """
     a_file = raw_input('Path to Audio file')
     try:
         repet = nussl.Repet(a_file)
         repet.run()
         rpt_period = repet.repeating_period
-        # TODO: do something with these.
+        # TODO: do something neat with these.
         fg, bg = repet.make_audio_signals()
     except NoBackendError:
         print 'No backend for selected media format, please ensure ffmpeg is installed.'
@@ -33,5 +40,5 @@ def process_lyrics():
 
 if __name__ == '__main__':
     # TODO: do that thing...
-    process_file()
-    process_lyrics()
+    lyrics = process_lyrics()
+    process_audio(lyrics)
